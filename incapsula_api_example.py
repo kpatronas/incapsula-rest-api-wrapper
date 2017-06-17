3d45603cfac6f1848f29a3bd9e832a375c69e5ea
Konstantinos Patronas <kpatronas@gmail.com>
2017-06-17 16:49:10 +0300

    Update incapsula_api_example.py

--- incapsula_api_example.py
+++ incapsula_api_example.py
@@ -2,35 +2,21 @@
 import requests
 import time
 import sys
-from incapsula import stats
+from incapsula import *
 
 if __name__ == '__main__':
 
     # Create the stats object
-    s = stats(api_id='1234567890', api_key='1234567890')
+    s = incapsula(api_id='1234567890', api_key='1234567890')
 
     # Example of the stats api using custom timeranges
     # More info at https://my.incapsula.com/api/docs/v1/data
-    print s.visits_timeseries(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.hits_timeseries(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.bandwidth_timeseries(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.requests_geo_dist_summary(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.visits_dist_summary(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.caching(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.caching_timeseries(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.threats(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.incap_rules(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-    print s.incap_rules_timeseries(account_id='111111', site_id='222222', start='1-06-2017 00:00:00', end='3-06-2017 00:00:00', time_range='custom')
-
-    # Example of the stats api using pre-defined timeranges
-    # More info at https://my.incapsula.com/api/docs/v1/data
-    print s.visits_timeseries(account_id='111111', site_id='222222', time_range='today')
-    print s.hits_timeseries(account_id='111111', site_id='222222', time_range='today')
-    print s.bandwidth_timeseries(account_id='111111', site_id='222222', time_range='today')
-    print s.requests_geo_dist_summary(account_id='111111', site_id='222222', time_range='today')
-    print s.visits_dist_summary(account_id='111111', site_id='222222', time_range='today')
-    print s.caching(account_id='111111', site_id='222222', time_range='today')
-    print s.caching_timeseries(account_id='111111', site_id='222222', time_range='today')
-    print s.threats(account_id='111111', site_id='222222', time_range='today')
-    print s.incap_rules(account_id='111111', site_id='222222', time_range='today')
-    print s.incap_rules_timeseries(account_id='111111', site_id='222222', time_range='today')
+    try:
+        print s.stats(account_id='111111',
+                      site_id='222222',
+                      stats = 'visits_timeseries',
+                      start='1-06-2017 00:00:00',
+                      end='3-06-2017 00:00:00',
+                      time_range='custom')
+    except Exception as ex:
+        print ex
